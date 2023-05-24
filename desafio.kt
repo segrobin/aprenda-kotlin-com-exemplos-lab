0758f2cd52fb3fcfc30cd015fdbdbd81a072e022
gabrielsb <113694878+segrobin@users.noreply.github.com>
2023-05-24 16:59:33 -0300

    Update desafio.kt
    COMIT CODIGO FINAL

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,37 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Aluno(val nome: String)
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+    val alunosMatriculados = mutableListOf<Aluno>()
+
+    fun matricularAluno(vararg alunos: Aluno) {
+        alunosMatriculados.addAll(alunos)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+    val conteudo3 = ConteudoEducacional("Banco de Dados", 90)
+    val conteudo4 = ConteudoEducacional("Integração com o banco de dados",30)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+    val formacaoJava = Formacao("Formação Java", Nivel.DIFICIL, listOf(conteudo2, conteudo3,conteudo4))
+
+    val aluno1 = Aluno("João Sampaio lima")
+    val aluno2 = Aluno("Maria Jose barbosa")
+    val aluno3 = Aluno("Pedro Lima Souza")
+    val aluno4 = Aluno("Gabriel Simoes Borges")
+
+    formacaoKotlin.matricularAluno(aluno1, aluno2)
+    formacaoJava.matricularAluno(aluno2, aluno3 , aluno4)
+
+    println("Alunos matriculados na formação ${formacaoKotlin.nome}:")
+    formacaoKotlin.alunosMatriculados.forEach { println(it.nome) }
+
+    println("Alunos matriculados na formação ${formacaoJava.nome}:")
+    formacaoJava.alunosMatriculados.forEach { println(it.nome) }
 }
